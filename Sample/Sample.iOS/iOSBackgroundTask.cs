342164a1bd48b596a8ccf35fe43aa4f90c4f3be0
Rastorguev <rastorguevs@gmail.com>
2017-09-11 11:57:56 +0300

    Implement infinite background task on iOS

--- Sample/Sample.iOS/iOSBackgroundTask.cs
+++ Sample/Sample.iOS/iOSBackgroundTask.cs
@@ -1,24 +1,77 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using Sample.Core;
+using UIKit;
 
 namespace Sample.iOS
 {
     internal class iOSBackgroundTask : IBackgroundTask
     {
+        private nint _taskId;
+        private CancellationTokenSource _cancellationTokenSource;
+
         public bool IsRunning { get; private set; }
 
-        public void Start()
+        public async void Start()
         {
+            if (IsRunning)
+            {
+                return;
+            }
+
+            _cancellationTokenSource = new CancellationTokenSource();
+            var cancellationToken = _cancellationTokenSource.Token;
+
+            _taskId = UIApplication.SharedApplication.BeginBackgroundTask(() =>
+            {
+                Console.WriteLine("------- Background execution time expired -------");
+                UIApplication.SharedApplication.EndBackgroundTask(_taskId);
+            });
+
             IsRunning = true;
 
-            Debug.WriteLine("------- Start -------");
+            Debug.WriteLine("------- Background task started -------");
+
+            try
+            {
+                await DoWork(cancellationToken);
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.WriteLine("------- Background task canceled -------");
+            }
+
+            Debug.WriteLine("------- Background task completed -------");
+
+            UIApplication.SharedApplication.EndBackgroundTask(_taskId);
         }
 
         public void Stop()
         {
-            IsRunning = false;
+            if (IsRunning)
+            {
+                _cancellationTokenSource?.Cancel();
 
-            Debug.WriteLine("------- Stop -------");
+                IsRunning = false;
+            }
+
+            Debug.WriteLine("------- Background task stopped -------");
+        }
+
+        private async Task DoWork(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var timeRemaining = UIApplication.SharedApplication.BackgroundTimeRemaining;
+
+                Debug.WriteLine($"------- Time Remaining: {timeRemaining} -------");
+
+                await Task.Delay(1000, cancellationToken);
+            }
         }
     }
 }
